567b7f3f6cb810022b316d6b0d8ec3f234ff1bd6
yasser2020 <yezzat2020@gmail.com>
2022-05-13 22:01:56 +0200

    split data into training_data and testing_data using train_test_split

--- LinearRegression.py
+++ LinearRegression.py
@@ -2,30 +2,25 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import linear_model
+from sklearn.model_selection import train_test_split
 
-cells=pd.read_csv('cells.csv')
+cells=pd.read_csv('data/cells.csv')
 
 x_train=cells[['time']]
 y_train=cells[['cells']]
 
+print(x_train)
+x_train,x_test,y_train,y_test=train_test_split(x_train,y_train,test_size=0.4,random_state=10)
+
 #create instance of model
 
 reg=linear_model.LinearRegression()
+reg.fit(x_train,y_train)
+predication_test=reg.predict(x_test)
+print(predication_test)
+print(y_test)
+print("Mean squre error mean = ",np.mean(predication_test - y_test)**2)
 
-#train model 
+plt.scatter(predication_test,predication_test - y_test)
+plt.hlines(y=0, xmin=200, xmax=310)
 
-reg.fit(x_train, y_train)
-
-#print("Predicted # cells : ",reg.predict([[2.3]]))
-#print(reg.coef_)
-#print(reg.intercept_)
-
-cells_predict_df=pd.read_csv('cells_predict.csv')
-predicted_cells=reg.predict(cells_predict_df)
-
-cells_predict_df['cells']=predicted_cells
-
-cells_predict_df.to_csv('test.csv')
-
-cel=pd.read_csv('test.csv')
-print(cel.head())
